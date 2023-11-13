3b2cc98ab77c8c72670618b669169e58601cf649
Jobijoseph <jobijoseph118@gmail.com>
2023-11-13 15:14:50 +0530

    crud app

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name:"yousaf Khan",
+        email:"yousaft1@gmail.com",
+        id:4
+    },
+    {
+        name:"Khalil",
+        email:"Khalil12@gmail.com",
+        id:3
+    },
+    {
+        name:"Akahs",
+        email:"Akash2@gmail.com",
+        id:1
+    }
+]
